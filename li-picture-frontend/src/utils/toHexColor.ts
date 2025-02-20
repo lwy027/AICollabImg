e723565b51e1e4835915e996b57a6148dc13159b
liweiye <2759536538@qq.com>
2025-02-20 13:04:55 +0800

    图片功能扩展-前端

---
+++ li-picture-frontend/src/utils/toHexColor.ts
@@ -0,0 +1,10 @@
+export default function toHexColor(input) {
+  // 去掉 0x 前缀
+  const colorValue = input.startsWith('0x') ? input.slice(2) : input
+
+  // 将剩余部分解析为十六进制数，再转成 6 位十六进制字符串
+  const hexColor = parseInt(colorValue, 16).toString(16).padStart(6, '0')
+
+  // 返回标准 #RRGGBB 格式
+  return `#${hexColor}`
+}
